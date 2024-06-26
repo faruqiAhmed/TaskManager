0beee46bd66450215c7644e5410058f0f1c16837
Md.Omar Faruq <43860406+faruqiAhmed@users.noreply.github.com>
2024-06-26 22:54:15 +0600

    Created data Model

---
+++ TaskManager/ViewController/ToDoListViewController.swift
@@ -0,0 +1,23 @@
+//
+// ToDoListViewController.swift
+//  TaskManager
+//
+//  Created by Md Omar Faruq on 26/6/24.
+//
+
+import UIKit
+import CoreData
+
+class ToDoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Task Manager"
+        let task =  Task()
+        
+    }
+
+
+}
+
