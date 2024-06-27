cb6b23284e959ed8b43bbd995d069e5780ada663
Md.Omar Faruq <43860406+faruqiAhmed@users.noreply.github.com>
2024-06-27 11:09:45 +0600

    Created Core Data Manager initialization

--- TaskManager/Resources/CoreDataManager.swift
+++ TaskManager/Resources/CoreDataManager.swift
@@ -7,3 +7,33 @@
 
 import CoreData
 import Foundation
+
+class CoreDataManager {
+   static let shared = CoreDataManager()
+    
+    private init() {}
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Taks")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error\(error),\(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                print("Error saving the staged changes \(nserror),\(nserror.userInfo)")
+            }
+        }
+    }
+}
